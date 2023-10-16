5d20fb3ffbdb3ce7954ada1d8440c76b5583476c
luis-loredo <luis.loredoo@uanl.edu.mx>
2023-10-16 13:55:50 -0600

    update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import sys
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 if __name__ == "__main__":
     a, b = sys.argv[1], sys.argv[2] # python execute param1
+    print(a, b)
+    print(add(a, b))
+    print(sub(a, b))
     
